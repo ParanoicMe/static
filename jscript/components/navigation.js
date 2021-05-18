7af7aebbfe7decd549e41ee4b492289a1b20a314
Denis <ZarubaikoDenis@gmail.com>
2021-05-18 15:57:53 +0300

    Update navigation.js

--- jscript/components/navigation.js
+++ jscript/components/navigation.js
@@ -1,4 +1,5 @@
-$(document).ready(function () {
+navigation.left = function() {
+	var that = table;
 	if(page!='protocols'){
 		$("#dropdownmenu").css('display', 'none');
 	}else{
@@ -13,4 +14,34 @@ $(document).ready(function () {
 	$("#roles").click(function () {
 		window.location.href = "/HU-Web/roles";
 	});
-});	
+	$("#statistic").click(function () {
+        	window.location.href = "/HU-Web/history";
+    	});
+	$("#requests").click(function () {
+        	window.location.href = "/HU-Web/requests";
+    	});
+	$("#claimed").click(function () {
+		that.field.filter = "claimed";
+		that.init("list.do", that.fields);
+	});
+	$("#priceAccept").click(function () {
+		that.field.filter = "priceAccept";
+		that.init("list.do", that.fields);
+	});
+	$("#processing").click(function () {
+		that.field.filter = "processing";
+		that.init("list.do", that.fields);
+	});
+	$("#processed").click(function () {
+		that.field.filter = "processed";
+		that.init("list.do", that.fields);
+	});
+	$("#rejected").click(function () {
+		that.field.filter = "rejected";
+		that.init("list.do", that.fields);
+	});
+	$("#paused").click(function () {
+		that.field.filter = "paused";
+		that.init("list.do", that.fields);
+	});
+}
